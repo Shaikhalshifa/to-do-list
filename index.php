3edb5adbbd7b65c45a87ae6e4d28badaf1578661
Alshifa shafik shaikh <110462759+Shaikhalshifa@users.noreply.github.com>
2023-02-10 13:54:32 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,88 @@
+<?php 
+    // initialize errors variable
+    $errors = "";
+
+    // connect to database
+    $db = mysqli_connect("localhost", "root", "root", "todo");
+
+    // insert a quote if submit button is clicked
+    if (isset($_POST['submit'])) {
+        if (empty($_POST['task'])) {
+            $errors = "You must fill in the task";
+        }else{
+            $task = $_POST['task'];
+            $sql = "INSERT INTO tasks (task) VALUES ('$task')";
+            mysqli_query($db, $sql);
+            header('location: index.php');
+        }
+    }   
+
+    // ...
+
+   if (isset($_GET['del_task'])) {
+    $id = $_GET['del_task'];
+
+    mysqli_query($db, "DELETE FROM tasks WHERE id=".$id);
+    header('location: index.php');
+}
+
+?>
+<?php 
+    if (isset($_GET['edit'])) {
+        $id = $_GET['edit'];
+        $update = true;
+        $record = mysqli_query($db, "SELECT * FROM info WHERE id=$id");
+
+        if (count($record) == 1 ) {
+            $n = mysqli_fetch_array($record);
+            $name = $n['name'];
+            $address = $n['address'];
+        }
+    }
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>ToDo List Application PHP and MySQL</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <div class="heading">
+        <h2 style="font-style: 'Hervetica';">ToDo List Application PHP and MySQL database</h2>
+    </div>
+    <form method="post" action="index.php" class="input_form">
+        <input type="text" name="task" class="task_input">
+        <button type="submit" name="submit" id="add_btn" class="add_btn">Add Task</button>
+        <?php if (isset($errors)) { ?>
+    <p><?php echo $errors; ?></p>
+<?php } ?>
+    </form>
+    <table>
+    <thead>
+        <tr>
+            <th>N</th>
+            <th>Tasks</th>
+            <th style="width: 60px;">Action</th>
+        </tr>
+    </thead>
+
+    <tbody>
+        <?php 
+        // select all tasks if page is visited or refreshed
+        $tasks = mysqli_query($db, "SELECT * FROM tasks");
+
+        $i = 1; while ($row = mysqli_fetch_array($tasks)) { ?>
+            <tr>
+                <td> <?php echo $i; ?> </td>
+                <td class="task"> <?php echo $row['task']; ?> </td>
+                <td class="delete"> 
+                    <a href="index.php?del_task=<?php echo $row['id'] ?>">x</a> 
+                </td>
+            </tr>
+        <?php $i++; } ?>    
+    </tbody>
+</table>
+</body>
+</html>
